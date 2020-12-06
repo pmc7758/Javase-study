0cb64e42d7943e65c6a2b778aef9a89db820f6bf
pmc7758 <614039502@qq.com>
2020-12-06 09:55:17 +0800

    解释环境变量配置实例

---
+++ 环境变量配置/Test.java
@@ -0,0 +1,17 @@
+public class Test
+{
+	float phonePrice=888.88F;
+
+	int a='C';
+
+	public static void main(String args[]){
+		System.out.println("桂电");
+	}
+}
+/*
+
+在控制台输出Hello World
+
+控制台（console）：
+
+*/
